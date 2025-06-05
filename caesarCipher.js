ce75e7fe6fa9152fa3bb7798bd8dfed6f9cf52d7
Artem <161304626+RTJIL@users.noreply.github.com>
2025-06-05 13:31:40 +0000

    next: write tests add: caesarCipher function

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,3 +1,27 @@
-function caesarCipher(a, b) {}
+/**
+ *
+ * @param {string} str
+ * @param {number} sf
+ */
+function caesarCipher(str, sf) {
+  let result = '';
 
-export { caesarCipher };
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    let code = str.charCodeAt(i);
+
+    if (char >= 'a' && char <= 'z') {
+      let shifted = (((code - 97 + sf) % 26) + 26) % 26;
+      result += String.fromCharCode(97 + shifted);
+    } else if (char >= 'A' && char <= 'Z') {
+      let shifted = (((code - 65 + sf) % 26) + 26) % 26;
+      result += String.fromCharCode(65 + shifted);
+    } else {
+      result += char; // leave symbols unchanged
+    }
+  }
+
+  return result;
+} // hello, 2 =>
+
+export { caesarCipher }
